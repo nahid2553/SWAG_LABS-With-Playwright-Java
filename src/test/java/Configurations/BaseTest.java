54234a700dba21c605f57d41a2d1bfe0fcc9c777
S M Nahid Iqbal <nahid152553@gmail.com>
2025-11-20 01:50:02 +0600

    Updated BaseTest and Config class

--- src/test/java/Configurations/BaseTest.java
+++ src/test/java/Configurations/BaseTest.java
@@ -1,4 +1,40 @@
 package Configurations;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import static Utilities.Config.*;
+
 public class BaseTest {
+    protected Browser browser;
+    protected Page page;
+    protected BrowserContext context;
+
+    //Browser Initialization
+    @BeforeEach
+    public void setUp(){
+        Playwright playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(isHeadlessMode()));
+
+        context = browser.newContext(new Browser.NewContextOptions()
+                .setViewportSize(VIEWPORT_WIDTH,VIEWPORT_HEIGHT));
+        page = context.newPage();
+    }
+    //Resources cleanup
+    @AfterEach
+    public void tearDown(){
+        if (page != null){
+            page.close();
+        }
+        if (context != null){
+            context.close();
+        }
+        if (browser != null){
+            browser.close();
+        }
+
+    }
+    //Common Test Method
+
+
 }
---
+++ src/test/java/Utilities/Config.java
@@ -0,0 +1,45 @@
+package Utilities;
+
+import com.microsoft.playwright.Page;
+
+public class Config {
+
+    //Url Configuration
+    public static final String BASE_URL = "https://www.saucedemo.com/";
+//    page.navigate(Config.getBaseUrl() + "/login"); //demo for next url
+
+    //Browser Configuration
+    public static final String BROWSER = "chromium";
+    public static final boolean HEADLESS = false;
+    public static final int BROWSER_TIMEOUT = 30000;
+
+    //Wait configuration
+    public static final int TIMEOUT = 5000;
+    public static final int EXPLICIT_WAIT = 10000;
+
+    //Window View Size
+    public static final int VIEWPORT_WIDTH = 1080;
+    public static final int VIEWPORT_HEIGHT = 800;
+
+//    public static final String SCREENSHOT_PATH = "screenshots/";
+//    public static final String REPORT_PATH = "test-reports/";
+//    public static final String LOG_PATH = "logs/";
+//
+//    public static final String VALID_USERNAME = "standard_user";
+//    public static final String VALID_PASSWORD = "secret_sauce";
+//    public static final String INVALID_USERNAME = "invalid_user";
+//    public static final String INVALID_PASSWORD = "wrong_password";
+
+    public static String getBaseUrl() {
+        String envUrl = System.getenv("BASE_URL");
+        return (envUrl != null && !envUrl.isEmpty()) ? envUrl : BASE_URL;  //if true : if false
+    }
+
+    public static boolean isHeadlessMode() {
+        String headless = System.getenv("HEADLESS");
+        if (headless != null) {
+            return Boolean.parseBoolean(headless);  //Boolean.parseBoolean()` = Convert text to true/false
+        }
+        return HEADLESS;
+    }
+}
